7f68b088c23d7f4760f7b74867608b15b433d8d4
Ian Macalinao <github@igm.pub>
2021-02-21 13:19:18 -0600

    Update readme

--- tasks/deploy-create2.ts
+++ tasks/deploy-create2.ts
@@ -1,5 +1,4 @@
 import { CeloWallet } from "@celo-tools/celo-ethers-wrapper";
-import { CeloContract } from "@celo/contractkit";
 import {
   deployerAddress,
   deployerKey,
@@ -13,16 +12,19 @@ export const deployCreate2: ActionType<{}> = async (_, hre) => {
     throw new Error("Deployer not found.");
   }
 
-  // send $1 to funding addr
+  console.log("Send $0.10 to funding addr");
   const cUSD = await hre.celo.kit.contracts.getStableToken();
-  await cUSD.transfer(deployerAddress, "100000000000000000").send({
-    from: await deployer.getAddress(),
-    gas: 5000000,
-    gasPrice: 0.5 * 10 ** 9,
-    feeCurrency: CeloContract.StableToken,
-  });
+  await (
+    await cUSD.transfer(deployerAddress, "10000000000000000").send({
+      from: await deployer.getAddress(),
+      gas: 5000000,
+      gasPrice: 10 ** 9,
+      feeCurrency: cUSD.address,
+    })
+  ).waitReceipt();
 
   // deploy create2 factory
+  console.log("Deploy CREATE2 factory");
   const provider = hre.celo.ethersProvider;
   const wallet = new CeloWallet(deployerKey, provider);
   const tx = await (
